b67129586e290a6b7ac91d6aa26810befd69f173
Paul Borawski <paulborawski14@gmail.com>
2016-04-14 16:26:58 -0700

    got the ball rolling

--- pong.js
+++ pong.js
@@ -1,14 +1,19 @@
 var canvas;
 var canvasContext;
-
-
-
+var ballX = 50;
 
 window.onload = function() {
-  console.log("Window has loaded");
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
+
+  var framesPerSecond = 30;
+  setInterval(drawEverything, 1000/framesPerSecond);
+}
+
+function drawEverything() {
+  ballX = ballX + .5;
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
-
+  canvasContext.fillStyle = 'red';
+  canvasContext.fillRect(ballX,100,10,10);
 }
