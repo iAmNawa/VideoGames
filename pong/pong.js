9abbdb1518d876d8d2ed07f2a19323fbab457cb7
Paul Borawski <paulborawski14@gmail.com>
2016-04-14 16:41:36 -0700

    new folder

--- pong/pong.js
+++ pong/pong.js
@@ -7,13 +7,21 @@ window.onload = function() {
   canvasContext = canvas.getContext('2d');
 
   var framesPerSecond = 30;
-  setInterval(drawEverything, 1000/framesPerSecond);
+  setInterval(function() {
+    moveEverything();
+    drawEverything();
+  }, 1000/framesPerSecond);
+}
+
+function moveEverything() {
+  ballX = ballX + .5;
 }
 
 function drawEverything() {
-  ballX = ballX + .5;
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(0,210,10,100);
   canvasContext.fillStyle = 'red';
   canvasContext.fillRect(ballX,100,10,10);
 }
