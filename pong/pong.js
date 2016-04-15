fbab574e3cbc3a757dc0d84922f0b365f76a7be6
Paul Borawski <paulborawski14@gmail.com>
2016-04-14 18:28:53 -0700

    both paddles working

--- pong/pong.js
+++ pong/pong.js
@@ -5,7 +5,9 @@ var ballSpeedX = 10;
 var ballY = 50;
 var ballSpeedY = 4;
 
-var paddle1Y= 250;
+var paddle1Y = 250;
+var paddle2Y = 250;
+const PADDLE_THICKNESS = 10;
 const PADDLE_HEIGHT = 100;
 
 function calculateMousePos(evt) {
@@ -33,19 +35,33 @@ window.onload = function() {
   canvas.addEventListener('mousemove',
     function(evt) {
       var mousePos = calculateMousePos(evt);
-      paddle1Y = mousePos.y;
+      paddle1Y = mousePos.y-(PADDLE_HEIGHT/2);
     })
+}
+
+function ballReset() {
+  ballSpeedX = -ballSpeedX;
+  ballX = canvas.width/2;
+  ballY = canvas.height/2;
 }
 
 function moveEverything() {
   ballX = ballX + ballSpeedX;
   ballY = ballY + ballSpeedY;
-  if (ballX > canvas.width) {
-    ballSpeedX = -ballSpeedX;
-  }
   if (ballX < 0) {
-    ballSpeedX = -ballSpeedX;
+    if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
+        ballSpeedX = -ballSpeedX;
+    } else {
+        ballReset();
+      }
   }
+  if (ballX > canvas.width) {
+    if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
+        ballSpeedX = -ballSpeedX;
+    } else {
+        ballReset();
+      }
+    }
   if (ballY > canvas.width) {
     ballSpeedY = -ballSpeedY;
   }
@@ -58,7 +74,9 @@ function drawEverything() {
   // blanks out canvas with black
   colorRect(0,0,canvas.width,canvas.height,'black');
   //left player paddle
-  colorRect(0,paddle1Y,10,100,'white');
+  colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
+  //right player paddle
+  colorRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
   //draws the ball
   colorCircle(ballX, ballY, 10, 'white');
 }
