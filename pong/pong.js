09acd0735fb209d065b23ce760c90a5aa436cbeb
Paul Borawski <paulborawski14@gmail.com>
2016-04-14 19:05:45 -0700

    scoring

--- pong/pong.js
+++ pong/pong.js
@@ -5,10 +5,13 @@ var ballSpeedX = 10;
 var ballY = 50;
 var ballSpeedY = 4;
 
+var player1Score = 0;
+var player2Score = 0;
+
 var paddle1Y = 250;
 var paddle2Y = 250;
 const PADDLE_THICKNESS = 10;
-const PADDLE_HEIGHT = 100;
+const PADDLE_HEIGHT = 250;
 
 function calculateMousePos(evt) {
   var rect = canvas.getBoundingClientRect();
@@ -45,14 +48,25 @@ function ballReset() {
   ballY = canvas.height/2;
 }
 
+function computerMovement() {
+  var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
+  if (paddle2YCenter < ballY-35) {
+    paddle2Y += 6;
+} else if (paddle2YCenter > ballY+35){
+  paddle2Y -= 6;
+}
+}
 function moveEverything() {
-  ballX = ballX + ballSpeedX;
-  ballY = ballY + ballSpeedY;
+  computerMovement();
+
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
   if (ballX < 0) {
     if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
         ballSpeedX = -ballSpeedX;
     } else {
         ballReset();
+        player2Score++;
       }
   }
   if (ballX > canvas.width) {
@@ -60,12 +74,13 @@ function moveEverything() {
         ballSpeedX = -ballSpeedX;
     } else {
         ballReset();
+        player1Score++;
       }
     }
-  if (ballY > canvas.width) {
+  if (ballY > canvas.height) {
     ballSpeedY = -ballSpeedY;
   }
-  if (ballX < 0) {
+  if (ballY < 0) {
     ballSpeedY = -ballSpeedY;
   }
 }
@@ -79,6 +94,9 @@ function drawEverything() {
   colorRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
   //draws the ball
   colorCircle(ballX, ballY, 10, 'white');
+
+  canvasContext.fillText(player1Score,100,100)
+  canvasContext.fillText(player2Score,canvas.width-100,100)
 }
 
 function colorCircle(centerX, centerY, radius, drawColor) {
